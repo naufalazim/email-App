58dde65f51176332b020bfd3a424a1deb775eba0
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-12-18 21:04:22 +0800

    change text

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -5,7 +5,7 @@ public class EmailApp {
 	
 	public static void main(String[] args) {
 		// run app:
-		System.out.println("SELAMAAT DATANG");
+		System.out.println("SELAMAAT DATANG KE EMAIL APP");
 		Email email = new Email("Naufal", "Azim");
 		
 	}

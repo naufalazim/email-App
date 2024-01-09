ce4a94f6ef60c4d8cad539329b5c4d0ca92bff77
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2024-01-09 22:06:15 +0800

    Add features

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -5,8 +5,13 @@ public class EmailApp {
 	
 	public static void main(String[] args) {
 		// run app:
-		System.out.println("SELAMAAT DATANG KE EMAIL APP");
+		System.out.println("SELAMAAT DATANG KE EMAIL Generator App");
 		Email email = new Email("Naufal", "Azim");
+		
+		email.setAlternateEmail("nxfal.com");
+		
+		//return email:
+		System.out.println(email.getAlternateEmail());
 		
 	}
 }
